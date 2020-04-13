0d6528a714e1d3a04343f3326a8a0164b677bd2d
FrozenCz <Milan12as36>
2020-04-13 11:17:32 +0200

    prvni ukol

---
+++ 1/app.js
@@ -0,0 +1,121 @@
+﻿//////////////////////////////////////////////////////////////
+//
+//  Zadání 1. cvičení, app.js
+//
+//////////////////////////////////////////////////////////////
+//
+// 1. Inicializujte projekt: 
+//    Ujistěte se, že aktuální adresář v okně terminálu je totožný s adresářem, kde se nachází soubor app.js
+//    V okně terminálu spusťtě příkaz npm init -y
+//    
+//    Výsledkem je popis projektu v souboru package.json
+//
+// 2. na stránkách npmjs.com najdete modul yargs a číslo jeho současné verze
+// 3. v terminálu instalujte modul yargs příkazem npm i yargs@15.1.0
+//    Výsledkem je nový podadresář node_modules a soubor package-lock.json, jakož i aktualizovaný package.json
+//
+// 
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const fs = require('fs')
+const yargs = require('yargs')
+const notes = require('./notes')
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Aplikaci chceme pouštět příkazem node app.js add či node app.js remove a podobně
+// Pro parsování vstup z konzole použijeme yargs
+//
+// 5. Pro argumenty příkazové řádky jako je add a remove vytvoříme pomocí příkazu yargs.command podporu:
+//    Dopracujte podle vzoru pro příkaz add a podle komentářů podporu pro argumenty remove, read, list
+//
+// 6. po dopracování otestujte aplikaci s různými argumenty
+//    - příkaz z příkazové řádky node app.js --help vypíše nápovědu k aplikaci
+//    - příkaz z příkazové řádky node app.js add --title="seznam" --body="jedna, dva tři" má do souboru notes.json přidat objekt
+//
+//////////////////////////////////////////////////////////////////////////
+
+
+
+// VZOR: podpora příkazu add
+yargs.command({
+    command: 'add',
+    describe: 'Přidání poznámky',
+    builder: {
+        title: {
+            describe: 'název poznámky',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'text poznámky',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    
+    handler: function(argv) {
+        notes.addNote(argv.title, argv.body)
+    }    
+})
+
+// podpora remove
+// Poznámku vyhledáme podle názvu, tzn., příkaz remove budeme spouštět s jedním argumentem. Proto v části builder bude jen specifikace title
+yargs.command({
+    command: 'remove',
+    describe: 'Smazání poznámky',
+    builder: {
+        title: {
+            describe: 'název poznámky',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function(argv) {
+         notes.removeNote(argv.title)
+    } 
+})
+
+// podpora read
+// Poznámku vyhledáme podle názvu
+yargs.command({
+    command: 'read',
+    describe: 'cterni poznamky',
+    builder: {
+        title: {
+            describe: 'název poznámky',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+// podpora list
+// Tento příkaz nepotřebuje další argumenty
+yargs.command({
+    command: 'list',
+    describe: 'seznam poznamek',
+    builder: {},
+    handler: function () {
+        notes.listNotes()
+    }
+    // dopracovat části describe, builder, handler
+})
+
+
+// TESTOVÁNÍ:
+// příkaz z příkazové řádky:
+//    node app.js add --title="seznam" --body="jedna, dva tři" 
+//    node app.js add --title="mujSeznam" --body="čtyři, pět, šest" 
+//    node app.js read --title="seznam"
+//    node app.js list                                                                 
+//    node app.js remove --title="mujSeznam"
+//    node app.js list                                                                 
+
+
+yargs.parse()
---
+++ 1/notes.js
@@ -0,0 +1,78 @@
+﻿//////////////////////////////////////////////////////////////
+//
+//  Zadání 1. cvičení, notes.js
+//
+//////////////////////////////////////////////////////////////
+const fs = require('fs')
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+const saveNotes = function (notes) {
+    const notesJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', notesJSON)
+}
+
+const addNote = function (title, body) {
+    const notes = loadNotes()
+
+    const existuje = (notes, title) => {
+        return !notes.some((note) => {
+            note.title == title
+        });
+    }
+
+    if (title.length > 0 && existuje(notes, title)) {
+        notes.push({title, body})
+        saveNotes(notes)   
+    }     
+}
+
+// 8. přepracovat simulaci na řádnou funkci podle komentáře
+const removeNote = function (title) {
+    const notes = loadNotes();
+
+    const indexNaSmazani = (notes , title) => {
+
+    }
+    console.log(indexNaSmazani(notes, title));
+    //notes.slice()
+    console.log('Mažu poznámku')
+    // načtení všech poznámek
+    // vyhledání všech poznámek, které mají být zachovány
+    // uložení poznámek
+}
+
+// 9. dopracovat simulaci funkce pro čtení a výpis do konzole jedné poznámky 
+const readNote = function(title) {
+    const notes = loadNotes();
+    const note = (notes, title) => {
+        return notes.find((note) => note.title == title);
+    }
+    console.log(note(notes, title));
+}
+
+// 10. dopracujte simulaci funkci pro čtení a výpis do konzole všech poznámek
+const listNotes = function() {
+    const notes = loadNotes();
+    notes.forEach((note) => {
+       console.log('Title: ' + note.title + 'Body: ' + note.body);
+    });
+}
+
+
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote,
+
+    // 11. doplnit export potřebných funkcí (bez ukládání a načítání ze souboru)
+}
