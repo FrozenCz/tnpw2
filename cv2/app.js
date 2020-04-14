691330fb2ade3e3ceab771dad3a3169e1f2189f1
FrozenCz <Milan12as36>
2020-04-14 10:15:21 +0200

    cviceni dva hototo

---
+++ cv2/app.js
@@ -0,0 +1,79 @@
+﻿//////////////////////////////////////////////////////////////
+//
+//  Zadání 2. cvičení, app.js
+//
+//  Přepis definice funkcí do arrow zápisu
+//
+//////////////////////////////////////////////////////////////
+
+
+const fs = require('fs')
+const yargs = require('yargs')
+const notes = require('./notes')
+
+yargs.command({
+    command: 'add',
+    describe: 'Přidání poznámky',
+    builder: {
+        title: {
+            describe: 'název poznámky',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'text poznámky',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => notes.addNote(argv.title, argv.body)
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Vymazání poznámky',
+    builder: {
+        title: {
+            describe: 'název poznámky',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    // 2. přepište do arrow notace
+    handler: (argv) => notes.removeNote(argv.title)
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Výpis poznámky podle názvu',
+    builder: {
+        title: {
+            describe: 'název poznámky',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    // 3. přepište do arrow notace
+    handler: (argv) => notes.readNote(argv.title)
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Výpis všech poznámek',
+    //builder: {},
+    // 4. přepište do arrow notace
+    handler: () => notes.listNotes()
+})
+
+
+// TESTOVÁNÍ:
+// příkaz z příkazové řádky:
+//    node app.js add --title="seznam" --body="jedna, dva tři" 
+//    node app.js add --title="mujSeznam" --body="čtyři, pět, šest" 
+//    node app.js read --title="seznam"
+//    node app.js list                                                                 
+//    node app.js remove --title="mujSeznam"
+//    node app.js list                                                                 
+
+
+yargs.parse()
---
+++ cv2/notes.js
@@ -0,0 +1,87 @@
+﻿//////////////////////////////////////////////////////////////
+//
+//  Zadání 2. cvičení, notes.js
+//
+//  Dopracovat funkci pro načtení a výpis jedné poznámky (soubor notes.js), přičemž funkce bude poznámku hledat podle zadaného názvu
+//  Dopracovat funkci pro výpis všech poznámek do konzole (soubor notes.js)
+//  Dopracovat všechny funkce tak, aby v případě chyby (poznámku se zadaným názvem nelze vytvořit/smazat/vypsat) byl uživatel informován 
+//  Přepsat definice funkcí do arrow zápisu
+//
+//////////////////////////////////////////////////////////////
+
+const fs = require('fs')
+
+// 1. přepište do arrow notace
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+// 2. přepište do arrow notace
+const saveNotes = (notes) => {
+    const notesJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', notesJSON)
+}
+
+// 3. přepište do arrow notace
+const addNote = (title, body) => {
+    const notes = loadNotes()
+	
+				// přepište do arrow notace
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+    if (duplicateNotes.length === 0) {
+        notes.push({title, body})
+        saveNotes(notes)
+    }else{
+        alert('zadaná název již existuje')
+    }
+}
+
+// 5. přepište do arrow notace
+const removeNote = (title) => {
+    const notes = loadNotes()
+
+				// přepište do arrow notace
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+    if (notesToKeep.length < notes.length) {
+        saveNotes(notesToKeep)
+    // 6. přidat chybovou hlášku
+    }
+}
+
+// 7. přepište do arrow notace
+const readNote = (title) => {
+    const notes = loadNotes();
+    const nalezenaPoznamka = notes.filter((note) => note.title === title)
+
+    if(nalezenaPoznamka) {
+        // 8. dopracovat nalezení poznámky s uvedeným názvem, použít arrow notaci, uvést i chybovou hlášku
+        console.log('Vypisuji poznámku s názvem', title)
+    } else {
+        console.log('nenalezena poznámka')
+    }
+}
+
+// 9. přepište do arrow notace
+const listNotes = () => {
+    const notes = loadNotes();
+    notes.forEach((note) => console.log(note));
+    // 10.  dopracovat výpis všech poznámek do konzole, použít arrow notaci 
+}
+
+
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    readNote: readNote,
+    listNotes: listNotes
+}
